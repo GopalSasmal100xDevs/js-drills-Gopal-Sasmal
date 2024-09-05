df5e287daf1c3d8ef5d290b98b326e30c41de583
gopalprojectx100 <gopal.sasmal.29.3@mountblue.tech>
2024-09-05 14:53:25 +0530

    Added persons-drill problem 2 solution

--- persons-drill/solution.js
+++ persons-drill/solution.js
@@ -11,3 +11,29 @@ export function getAllEmails(persons) {
   }
   return personsEmail;
 }
+
+// Q2   Implement a function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
+
+export function getHobbiesOfaSpecificAge(persons, age) {
+  if (!Array.isArray(persons)) {
+    console.log("First argument is not an array!");
+    return;
+  }
+
+  if (persons.length == 0) {
+    console.log("Array is empty!");
+    return;
+  }
+
+  if (!Number.isInteger(age)) {
+    console.log("Age is not a number!");
+    return;
+  }
+
+  for (let index = 0; index < persons.length; index++) {
+    const person = persons[index];
+    if (person && person.age === age) {
+      console.log(...person.hobbies);
+    }
+  }
+}
