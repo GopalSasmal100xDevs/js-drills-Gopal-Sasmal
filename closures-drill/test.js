1d5766eb09508e545a70a686eff3f3c736ae9648
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-09-09 10:52:38 +0530

    Add limiting function call

--- closures-drill/test.js
+++ closures-drill/test.js
@@ -1,10 +1,24 @@
-import { counterFactory, cacheFunction } from "./solution.js";
+import {
+  counterFactory,
+  cacheFunction,
+  limitFunctionCallCount,
+} from "./solution.js";
 
 // testing counterFactory function
 const counter = counterFactory();
 console.log(counter.increment());
 console.log(counter.decrement());
 console.log(counter.increment());
+
+// testing limit function call
+const printLanguage = limitFunctionCallCount((data) => {
+  console.log(data);
+}, 3);
+printLanguage("JavaScript");
+printLanguage("Java");
+printLanguage("C++");
+printLanguage("Rust");
+printLanguage("Python");
 
 // testing cache
 const multiplyWith100 = cacheFunction((val) => 100 * val);
