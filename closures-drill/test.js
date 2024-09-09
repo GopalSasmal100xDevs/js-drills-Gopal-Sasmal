ca48d27d21e1215068c3e151105badbfb01e383a
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-09-09 10:43:50 +0530

    Added Cache function

--- closures-drill/test.js
+++ closures-drill/test.js
@@ -1,7 +1,13 @@
-import { counterFactory } from "./solution.js";
+import { counterFactory, cacheFunction } from "./solution.js";
 
 // testing counterFactory function
 const counter = counterFactory();
 console.log(counter.increment());
 console.log(counter.decrement());
 console.log(counter.increment());
+
+// testing cache
+const multiplyWith100 = cacheFunction((val) => 100 * val);
+console.log(multiplyWith100(10));
+console.log(multiplyWith100(100));
+console.log(multiplyWith100(10));
